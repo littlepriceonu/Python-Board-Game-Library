63b7a4b23891a8b6f7a7cb3604d2fb5e07c42dbe
Little <63677850+littlepriceonu@users.noreply.github.com>
2021-08-31 02:51:26 -0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,27 @@
+from Game import BoardGame
+
+i = ["Testing Board", 
+    "0-1","0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1","0-1"
+    ,"0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1","0-1"
+    ,"0-1", "0-1", "1-1", "0-1", "0-1", "0-1", "0-1", "0-1","0-1"
+    ,"0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1","0-1"
+    ,"0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1","0-1"
+    ,"0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1", "0-1","0-1"]
+
+
+BG = BoardGame(i)
+
+loader = BG.makeloader(i, 9)
+
+print(BG.getxy(5, 4, i))
+
+BG.printloadertablepos(loader)
+
+print("----------------\n")
+
+BG.printloadertablehealth(loader)
+
+BG.save("sf")
+
+def setxy(self, x, y, table, setto):
+	table[(y-1) * self.exit + x] = setto
