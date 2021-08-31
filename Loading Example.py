16546aed9c16a1372a95a872494e432c117bfc09
littlepriceonu <littlepriceonu@icloud.com>
2021-08-31 03:04:02 -0700

    More Comments

--- Loading Example.py
+++ Loading Example.py
@@ -4,7 +4,7 @@ i = ["Testing Table"]
 
 BG = BoardGame(i)
 
-BG.loadfromfile("sf") ## sf Would be a file with the table in it
+BG.loadfromfile("sf") ## sf Would be a file with the table in it. See example.py for table example
 
 BG.printloadertablepos(BG.getloadertable())
 
